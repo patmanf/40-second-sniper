a8ee4e25e67e3e51bad55fb9599ca39c362869a2
patmanf <27874300+patmanf@users.noreply.github.com>
2022-02-26 20:48:00 -0600

    :3

---
+++ pat/trainedsniper.lua
@@ -0,0 +1,69 @@
+require "/scripts/vec2.lua"
+
+function init()
+	script.setUpdateDelta(30)
+	
+	stillTimer = 0
+	shootTime = 40
+	lastPos = mcontroller.position()
+	
+	parameters = {
+		movementSettings = {collisionEnabled = false},
+		damageTeam = {type = "indiscriminate"},
+		damageType = "IgnoresDef",
+		power = math.huge,
+		speed = 500,
+		processing = "?scalenearest=3;1",
+		periodicActions = {
+			{
+				["time"] = 0.22,
+				["repeat"] = false,
+				action = "sound",
+				options = {"/sfx/gun/sniper3.ogg"}
+			},
+			{
+				["time"] = 0.01,
+				["repeat"] = true,
+				action = "particle",
+				specification = {
+					type = "ember",
+					color = {0,0,0,0},
+					light = {100,100,50},
+					timeToLive = 0.01
+				}
+			}
+		}
+	}
+	
+	message.setHandler("pat_trainedsniper", function(_,isLocal)
+		if isLocal then fire() end
+	end)
+end
+
+function update(dt)
+	local pos = mcontroller.position()
+	
+	if vec2.eq(pos, lastPos) and world.type() ~= "unknown" and status.resource("health") > 0 then
+		stillTimer = stillTimer + dt
+		
+		if stillTimer >= shootTime then
+			stillTimer = shootTime - 1
+			fire(pos)
+		end
+	elseif stillTimer > 0 then
+		stillTimer = math.max(0, stillTimer - dt * 4)
+	end
+	
+	lastPos = pos
+end
+
+function fire(pos)
+	local angle = math.random(50, 80) / 180 * math.pi
+	if math.random(0, 1) == 0 then
+		angle = angle * -1
+	end
+	local vec = vec2.rotate({0, -1}, angle)
+	local fpos = vec2.add(pos or mcontroller.position(), vec2.mul(vec, -150))
+	
+	world.spawnProjectile("standardbullet", fpos, nil, vec, false, parameters)
+end
