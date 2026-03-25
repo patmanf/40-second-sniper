70dc2ad6c061c48af9218fed70b475dd75fe2a0c
patmanf <27874300+patmanf@users.noreply.github.com>
2026-03-25 17:19:02 -0500

    rewrite the whole thing

--- pat/trainedsniper.lua
+++ pat/trainedsniper.lua
@@ -1,67 +1,58 @@
 require "/scripts/vec2.lua"
 
+local Cfg
+local Random
+local LastPos
+local StillTimer = 0
+
 function init()
-	script.setUpdateDelta(30)
-	
-	stillTimer = 0
-	shootTime = 40
-	lastPos = mcontroller.position()
-	
-	parameters = {
-		movementSettings = {collisionEnabled = false},
-		damageTeam = {type = "indiscriminate"},
-		damageType = "IgnoresDef",
-		power = math.huge,
-		speed = 500,
-		processing = "?scalenearest=3;1",
-		periodicActions = {
-			{
-				["time"] = 0.22,
-				["repeat"] = false,
-				action = "sound",
-				options = {"/sfx/gun/sniper3.ogg"}
-			},
-			{
-				["time"] = 0.01,
-				["repeat"] = true,
-				action = "particle",
-				specification = {
-					type = "ember",
-					color = {0,0,0,0},
-					light = {100,100,50},
-					timeToLive = 0.01
-				}
-			}
-		}
-	}
-	
-	message.setHandler("pat_trainedsniper", fire)
+  Random = sb.makeRandomSource()
+  
+  Cfg = root.assetJson("/pat/trainedsniper.sussy")
+  Cfg.repeatSeconds = Cfg.shootSeconds - Cfg.repeatSeconds
+  
+  Cfg.angleRange[1] = math.rad(Cfg.angleRange[1])
+  Cfg.angleRange[2] = math.rad(Cfg.angleRange[2])
+  if Cfg.projectileParams.power == -1 then
+    Cfg.projectileParams.power = math.huge
+  end
+
+  LastPos = mcontroller.position()
+
+  script.setUpdateDelta(Cfg.updateDelta)
+  message.setHandler("pat_trainedsniper", fire)
 end
 
 function update(dt)
-	local pos = mcontroller.position()
-	
-	if vec2.eq(pos, lastPos) and world.type() ~= "unknown" and status.resource("health") > 0 then
-		stillTimer = stillTimer + dt
-		
-		if stillTimer >= shootTime then
-			stillTimer = shootTime - 1
-			fire(pos)
-		end
-	elseif stillTimer > 0 then
-		stillTimer = math.max(0, stillTimer - dt * 4)
-	end
-	
-	lastPos = pos
+  if world.getProperty("invinciblePlayers") or world.getProperty("nonCombat") then return end
+
+  local pos = mcontroller.position()
+
+  if vec2.eq(pos, LastPos) then
+    StillTimer = StillTimer + dt
+
+    if StillTimer >= Cfg.shootSeconds then
+      StillTimer = Cfg.repeatSeconds
+      fire()
+    end
+  else
+    StillTimer = 0
+    LastPos = pos
+  end
 end
 
-function fire(pos)
-	local angle = math.random(50, 80) / 180 * math.pi
-	if math.random(0, 1) == 0 then
-		angle = angle * -1
-	end
-	local vec = vec2.rotate({0, -1}, angle)
-	local fpos = vec2.add(pos or mcontroller.position(), vec2.mul(vec, -150))
-	
-	world.spawnProjectile("standardbullet", fpos, nil, vec, false, parameters)
+function fire()
+  local angle = Random:randf(Cfg.angleRange[1], Cfg.angleRange[2])
+
+  local aimVec = vec2.withAngle(angle, -1)
+  local spawnPos = vec2.mul(aimVec, -Cfg.distance)
+
+  if Random:randb() then
+    spawnPos[1] = -spawnPos[1]
+    aimVec[1] = -aimVec[1]
+  end
+
+  spawnPos = vec2.add(mcontroller.position(), spawnPos)
+
+  world.spawnProjectile(Cfg.projectileType, spawnPos, nil, aimVec, false, Cfg.projectileParams)
 end
